28058d05528837c9839f4c5069dc365a8a34b9e2
Rakesh <Rakesh@Rakesh-PC>
2018-03-16 00:48:35 +0530

    New Automation Project Structure

---
+++ UiAutomation/src/main/java/com/test/automation/UiAutomation/App.java
@@ -0,0 +1,13 @@
+package com.test.automation.UiAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
