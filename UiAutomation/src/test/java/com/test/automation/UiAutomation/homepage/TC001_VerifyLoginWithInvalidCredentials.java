e74728e3d36136dc0be08fecfeba8f5e5d289507
Rakesh <Rakesh@Rakesh-PC>
2018-03-18 00:32:48 +0530

    Added code for to verify login with invalid credentials !

--- UiAutomation/src/test/java/com/test/automation/UiAutomation/homepage/TC001_VerifyLoginWithInvalidCredentials.java
+++ UiAutomation/src/test/java/com/test/automation/UiAutomation/homepage/TC001_VerifyLoginWithInvalidCredentials.java
@@ -1,20 +1,36 @@
 package com.test.automation.UiAutomation.homepage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 public class TC001_VerifyLoginWithInvalidCredentials 
 
 {
+	WebDriver driver;
+@BeforeTest
 public void setup()
 {
-	WebDriver driver;
-	
+
 	System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//driver//geckodriver.exe");
 	driver=new FirefoxDriver();
 	driver.get("http://automationpractice.com/index.php");
+}
+@Test
+public void verifyloginwithinvalidcredentials() throws InterruptedException
+{
 	
-	
-	
+	driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
+	driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rakesh.iter10@gmail.com");
+	driver.findElement(By.xpath("//*[@id='passwd']")).sendKeys("SWIFT@0467");
+	driver.findElement(By.xpath("//*[@id='SubmitLogin']")).click();
+	Thread.sleep(3000);
+}
+
+public void endTest()
+{
+	driver.close();
 }
 }
